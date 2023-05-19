839e1c0b255acdd8c133cf412adfeb36047acea9
Novi Dwi Cahya <novidwicahya19@gmail.com>
2023-05-19 21:48:07 +0700

    feat: add assets and components

---
+++ app/index.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { StyleSheet, View, ScrollView, SafeAreaView, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, FONT, SIZES, icons } from '../constants';
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from '../components';
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
